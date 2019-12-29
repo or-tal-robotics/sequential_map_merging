46ff4e6bdfd09e895feda50392585e12c88cd3be
Or Tslil <ortslil64@gmail.com>
2019-12-29 23:00:40 +0200

    or update

--- statistics/stat.py
+++ statistics/stat.py
@@ -9,21 +9,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-data_de = pd.read_csv("MonteCarloStatistics_de.csv")
+data_de = pd.read_csv("MonteCarloStatistics_de_2.csv")
 data_de = data_de.values
-data_pf = pd.read_csv("MonteCarloStatistics_pf.csv")
+data_pf = pd.read_csv("MonteCarloStatistics_pf_2.csv")
 data_pf = data_pf.values
 avg_de = np.mean(data_de, axis=0)
 avg_pf = np.mean(data_pf, axis=0)
 best_run_pf = np.where(data_pf == np.min(data_pf[:,-1]))[0]
 best_run_de = np.where(data_de == np.min(data_de[:,-1]))[0]
+best_de_line = data_de[best_run_de,-1]*np.ones_like(avg_pf)
+
 plt.figure(1)
-plt.plot(avg_de, color = 'r', label = "DE")
+#plt.plot(avg_de, color = 'r', label = "DE")
 plt.plot(avg_pf, color = 'b', label = "PF")
+plt.plot(best_de_line, color = 'k', label = "DENDT (best one)")
 plt.legend()
-plt.xlabel("t")
+plt.xlabel("k")
 plt.ylabel("MSE")
-plt.title("100 Monte Carlo runs")
+#plt.title("100 Monte Carlo runs")
 
 plt.figure(2)
 plt.plot(data_pf[best_run_pf,:].reshape(-1), color = 'k', label = "PF")
