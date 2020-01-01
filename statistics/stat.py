b8cfe2359453b4895c784af01a595391cb885694
Or Tslil <ortslil64@gmail.com>
2020-01-01 14:15:10 +0200

    up

--- statistics/stat.py
+++ statistics/stat.py
@@ -9,9 +9,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-data_de = pd.read_csv("MonteCarloStatistics_de_2.csv")
+data_de = pd.read_csv("csv/MonteCarloStatistics_de_4.csv")
 data_de = data_de.values
-data_pf = pd.read_csv("MonteCarloStatistics_pf_2.csv")
+data_pf = pd.read_csv("csv/MonteCarloStatistics_pf_2.csv")
 data_pf = data_pf.values
 avg_de = np.mean(data_de, axis=0)
 avg_pf = np.mean(data_pf, axis=0)
@@ -20,7 +20,7 @@ best_run_de = np.where(data_de == np.min(data_de[:,-1]))[0]
 best_de_line = data_de[best_run_de,-1]*np.ones_like(avg_pf)
 
 plt.figure(1)
-#plt.plot(avg_de, color = 'r', label = "DE")
+plt.plot(avg_de, color = 'r', label = "DE")
 plt.plot(avg_pf, color = 'b', label = "PF")
 plt.plot(best_de_line, color = 'k', label = "DENDT (best one)")
 plt.legend()
