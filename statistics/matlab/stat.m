2aa1b7035086d48f8dcf80bcbea37f451c3438d0
ortslil64 <ortslil64@gmail.com>
2020-01-17 06:41:00 +0200

    update figs

--- statistics/matlab/stat.m
+++ statistics/matlab/stat.m
@@ -1,23 +1,34 @@
-data_pf = table2array(readtable('csv/map10/MonteCarloStatistics_pf_map10v2_kidnepped.csv'));
-data_de = table2array(readtable('csv/map10/MonteCarloStatistics_de_map10v2_kidnepped.csv'));
+data_pf = table2array(readtable('csv/map10/MonteCarloStatistics_pf_map10v3.csv'));
+data_de = table2array(readtable('csv/map10/MonteCarloStatistics_de_map10v3.csv'));
+data_pf = data_pf(:,2:end);
+data_de = data_de(:,2:end);
 mean_pf = mean(data_pf,1);
-mean_pf = mean_pf(2:end)
 mean_de = mean(data_de,1);
-mean_de = mean_de(2:end)
+s_pf = sum(data_pf,2);
+best_pf = data_pf(find(s_pf==min(s_pf)),:);
+s_de = sum(data_de,2);
+best_de = data_de(find(s_de==min(s_de)),:);
+
 x = 1:length(mean_pf);
 hold on;
 plot(x, mean_pf,'b','LineWidth', 1);
 plot(x, mean_de,'r','LineWidth', 1);
+% plot(x, best_pf,'b--','LineWidth', 1);
+% plot(x, best_de,'r--','LineWidth', 1);
 xlabel('k');
 ylabel('MSE');
 % ---- map5 ---- %
 % xlim([1,96]);
-% ylim([0,14.5])
+% ylim([0,14.5]);
 
 % ---- map3 ---- %
 % xlim([1,196]);
-% ylim([0,14.5])
+% ylim([0,14.5]);
+
+% ---- map4 ---- %
+% xlim([1,60]);
+% ylim([0,16]);
 
 % ---- map10 ---- %
-xlim([1,109]);
-ylim([0,20.5])
+xlim([1,70]);
+ylim([0,32]);
